1363b7059cb2a5a9a70af2fad46d93ce89963a72
Franklin069 <dellano08@gmail.com>
2023-12-05 13:47:27 -0300

    Criei tabela

--- src/index.js
+++ src/index.js
@@ -12,23 +12,31 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
-//rota para criar um item
-app.post("/item", async (req, res) => {
-  const dados = req.body;
-  await prisma.item.create({
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  if (users.length > 0) return res.status(200).send(users);
+  return res.status(404).send("No users found");
+});
+
+app.post("/user", async (req, res) => {
+  const data = req.body;
+  await prisma.user.create({
     data: {
-      nome: dados.nome,
+      name: data.name,
     },
   });
   return res.sendStatus(201);
 });
 
-//rota para listar todos os usuários
-app.get("/users", async (req, res) => {
-});
-
-//rota para buscar um usuário pelo nome
-app.get("/user/:nome", async (req, res) => {
+app.get("/user/:name", async (req, res) => {
+  const name = req.params.name;
+  const user = await prisma.user.findMany({
+    where: {
+      name: name,
+    },
+  });
+  if (user.length > 0) return res.status(200).send(user);
+  return res.status(404).send("No user found");
 });
 
 // Inicie o servidor na porta especificada
